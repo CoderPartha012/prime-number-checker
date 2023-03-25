9a64831fef8ad72acae6c023a0a222eb5f25309a
Partha Rakshit <104616945+CoderPartha012@users.noreply.github.com>
2023-03-26 05:21:45 +0530

    Add files via upload

---
+++ BusinessLogic.java
@@ -0,0 +1,10 @@
+public class BusinessLogic {
+  public Boolean validate(final Integer primeNumber) {
+    for (int i = 2; i < (primeNumber / 2); i++) {
+      if (primeNumber % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
---
+++ TestJunit.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+@RunWith(Parameterized.class)
+public class TestJunit {
+     private Integer inputNumber;
+     private Boolean expectedResult;
+     private BusinessLogic logic;
+
+     @Before
+     public void initialize() {
+        logic = new BusinessLogic();
+     }
+
+     public TestJunit(Integer inputNumber, 
+        Boolean expectedResult) {
+       System.out.println("TestJunit-> inputNumber:"+inputNumber+" expectedResult:"+expectedResult);
+        this.inputNumber = inputNumber;
+        this.expectedResult = expectedResult;
+     }
+
+     @Parameterized.Parameters
+     public static Collection<Object[]> primeNumbers() {
+       System.out.println("primeNumbers() is called");
+        return Arrays.asList(new Object[][] {
+           { 2, true },
+           { 6, false },
+           { 19, true },
+           { 22, false },
+           { 23, true }
+        });
+     }
+
+     @Test
+     public void testPrimeNumberChecker() {
+        System.out.println("Parameterized Number is : " + inputNumber);
+        assertEquals(expectedResult, logic.validate(inputNumber));
+     }
+
+}
